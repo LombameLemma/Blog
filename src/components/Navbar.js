b3d131597a804b0d5cc0311c336fbed40692e78a
Lombame Lemma <lombamelemma@gmail.com>
2026-02-28 17:27:30 +0300

    Navbar edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
+
 function Navbar() {
   return (
     <nav className="navbar">
       <h2>My Blog</h2>
       <div>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Blog</a>
-        <a href="#">Contact</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
       </div>
     </nav>
   );
